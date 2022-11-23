48959803fe6ea41c6df4c3f93210a4ede4ea69de
Mohsin Tahir <mohsintahir999@gmail.com>
2022-11-23 23:41:49 +0000

    init

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 app_id = '0620fd4d'
 app_key = 'cb6954f29368c1f234ebc4d102ea0a20	'
-ingredient = 'banana'
-meal_type='breakfast'
+# ingredient = 'banana'
+# meal_type='breakfast'
 # result = requests.get(f'https://api.edamam.com/search?q={ingredient}&mealType={meal_type}&app_id={app_id}&app_key={app_key}')
 # data = result.json()
 # res=data['hits']
