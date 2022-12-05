4e3c9e0061bae298440186b7ae4d34df3931a34e
Mohsin Tahir <mohsintahir999@gmail.com>
2022-12-05 09:11:08 +0000

    added new index page

--- app.py
+++ app.py
@@ -3,34 +3,37 @@ import  requests
 
 app = Flask(__name__)
 
-# app_id = '0620fd4d'
-# app_key = 'cb6954f29368c1f234ebc4d102ea0a20	'
-# ingredient = 'banana'
-# meal_type='breakfast'
-# result = requests.get(f'https://api.edamam.com/search?q={ingredient}&mealType={meal_type}&app_id={app_id}&app_key={app_key}')
-# data = result.json()
-# res=data['hits']
-# for i in res:
-#     #print(res[0]['recipe']['label'])
-#     print (i['recipe']['label'])
-#     print('')
-#     for i in res:
-#         print (i['recipe']['ingredientLines'])
-#         print('')
-
-
 @app.route("/")
 def index():
     return render_template("index.html", title="Welcome", heading="API Test")
 
-@app.route("/" ,methods=['GET', 'POST'])
+@app.route("/edamam")
+def edamam():
+    return render_template("edamam.html", title="Edamam", heading="API Test")
+
+@app.route("/spoonacular")
+def spoonacular():
+    return render_template("spoonacular.html", title="Edamam", heading="API Test")
+
+@app.route("/spoonacular")
+def spoonProcess():
+    api_key='e989ba4eca774666be46dfb04dfd23f5'
+    ingredients = request.form["ingredients"]
+    results=requests.get(f'https://api.spoonacular.com/recipes/findByIngredients?apiKey={api_key}&ingredients={ingredients}&ranking=1#')
+    res=results.json()
+    return render_template("spoonacularResults.html", title="Spoonacular", heading="Your Spoonacular Recipes", data=res)
+
+@app.route("/edamam" ,methods=['GET', 'POST'])
 def process():
     app_id = '0620fd4d'
     app_key = 'cb6954f29368c1f234ebc4d102ea0a20	'
     meal_type=request.form["mealtype"]
     ingredients = request.form["ingredients"]
     health=request.form["dietdropdown"]
-    result = requests.get(f'https://api.edamam.com/search?q={ingredients}&health={health}&mealType={meal_type}&app_id={app_id}&app_key={app_key}')
+    if health =="" or " ":
+         result = requests.get(f'https://api.edamam.com/search?q={ingredients}&mealType={meal_type}&app_id={app_id}&app_key={app_key}')
+    else:
+        result = requests.get(f'https://api.edamam.com/search?q={ingredients}&health={health}&mealType={meal_type}&app_id={app_id}&app_key={app_key}')
     data = result.json()
     res=data['hits']
     return render_template("ingredients.html", title="Suggested Recipes ", heading="Your Recipes :)", data=res)
