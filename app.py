b496991a0a2458e2526d3c931c96ace9e9f870d8
Mohsin Tahir <mohsintahir999@gmail.com>
2022-12-23 23:13:31 +0000

    updated main page and added cosmetic changes

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 from flask import Flask, request, render_template
-import  requests
+import requests
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return render_template("index.html", title="Welcome", heading="")
 
-@app.route("/edamam")
+
+@app.route("/recipe")
 def edamam():
-    return render_template("edamam.html", title="Edamam", heading="Enter the following for a Personalised Recipe")
+    return render_template(
+        "edamam.html",
+        title="Macro Meals",
+        heading="Enter the following for a Personalised Recipe",
+    )
 
 
 def convert_to_metric(value, unit):
@@ -55,36 +61,32 @@ def convert_to_metric(value, unit):
     # Return the converted value and unit
     return f"{value} {unit}"
 
-@app.route("/edamam" ,methods=['GET', 'POST'])
+
+@app.route("/recipe", methods=["GET", "POST"])
 def process():
-    app_id = '0620fd4d'
-    app_key = 'cb6954f29368c1f234ebc4d102ea0a20	'
-    meal_type=request.form["mealtype"]
+    app_id = "0620fd4d"
+    app_key = "cb6954f29368c1f234ebc4d102ea0a20	"
+    meal_type = request.form["mealtype"]
     ingredients = request.form["ingredients"]
-    health=request.form["dietdropdown"]
-    if health =="" or " ":
-         result = requests.get(f'https://api.edamam.com/search?q={ingredients}&mealType={meal_type}&app_id={app_id}&app_key={app_key}')
+    health = request.form["dietdropdown"]
+    if health == "" or " ":
+        result = requests.get(
+            f"https://api.edamam.com/search?q={ingredients}&mealType={meal_type}&app_id={app_id}&app_key={app_key}"
+        )
     elif meal_type == "" or " ":
-        result = requests.get(f'https://api.edamam.com/search?q={ingredients}&health={health}&app_id={app_id}&app_key={app_key}')
+        result = requests.get(
+            f"https://api.edamam.com/search?q={ingredients}&health={health}&app_id={app_id}&app_key={app_key}"
+        )
     else:
-        result = requests.get(f'https://api.edamam.com/search?q={ingredients}&health={health}&mealType={meal_type}&app_id={app_id}&app_key={app_key}')
+        result = requests.get(
+            f"https://api.edamam.com/search?q={ingredients}&health={health}&mealType={meal_type}&app_id={app_id}&app_key={app_key}"
+        )
     data = result.json()
-    res=data['hits']
-    return render_template("ingredients.html", title="Suggested Recipes ", heading="Your Recipes :)", data=res)
+    res = data["hits"]
+    return render_template(
+        "results.html", title="Suggested Recipes ", heading="Your Recipes", data=res
+    )
 
-
-
-@app.route("/spoonacular")
-def spoonacular():
-    return render_template("spoonacular.html", title="Edamam", heading="API Test")
-
-@app.route("/spoonacular")
-def spoonProcess():
-    api_key='e989ba4eca774666be46dfb04dfd23f5'
-    ingredients = request.form["ingredients"]
-    results=requests.get(f'https://api.spoonacular.com/recipes/findByIngredients?apiKey={api_key}&ingredients={ingredients}&ranking=1#')
-    res=results.json()
-    return render_template("spoonacularResults.html", title="Spoonacular", heading="Your Spoonacular Recipes", data=res)
 
 if __name__ == "__main__":  # pragma no cover
     app.run(debug=True)
